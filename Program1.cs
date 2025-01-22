3ad380b96d43b58df2b29068f9cab2ec3604ac4a
Sahil <sahilpandey412@gmail.com>
2025-01-23 00:09:02 +0530

    Program 1 and 2 added

---
+++ Program1.cs
@@ -0,0 +1,9 @@
+using System;
+class Program1{
+	static void Main(){
+		int birthyear = 2000;
+		int currentyear = 2024;
+		int age = currentyear - birthyear;
+		Console.WriteLine(&quot;Henry&#39;s Age is: &quot;+age);
+	}
+}
